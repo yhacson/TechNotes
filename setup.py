68e23a39cd6e65d09322330f5f15cfa25e53cea3
hacson <hacson@qq.com>
2020-04-27 19:38:40 +0800

    init mkdocs

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='TechNotes',
+    version='1.0',
+    author='YL',
+    author_email='1020827270@qq.com',
+    url='https://github.com/yyyyl/TechNotes',
+    
+    install_requires=[
+        'mkdocs>=1.1',
+        'mkdocs-bootswatch>=1.1',
+    ],
+)
